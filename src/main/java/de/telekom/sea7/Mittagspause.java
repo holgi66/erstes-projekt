25a0da1b887060d6f4607894edc47ff693d39347
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-01-14 10:48:37 +0100

    start für erstes-projekt über eclips

---
+++ src/main/java/de/telekom/sea7/Mittagspause.java
@@ -0,0 +1,11 @@
+package de.telekom.sea7;
+
+public class Mittagspause {
+
+	public static int square(int operand) {
+	int	ergebnis = operand * operand;
+       System.out.println("das ergebnis ist" + ergebnis);
+       return ergebnis;
+	}
+
+}
